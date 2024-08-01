7c1f644aa79db48a5ad4d2c3d5c039d65484e75d
reoring <reoring@craftsman-software.com>
2024-08-01 17:04:02 +0900

    chore: fix provider config

--- internal/clients/zitadel.go
+++ internal/clients/zitadel.go
@@ -20,29 +20,61 @@ import (
 
 const (
 	// error messages
-	errNoProviderConfig     = "no providerConfigRef provided"
-	errGetProviderConfig    = "cannot get referenced ProviderConfig"
-	errTrackUsage           = "cannot track ProviderConfig usage"
-	errExtractCredentials   = "cannot extract credentials"
-	errUnmarshalCredentials = "cannot unmarshal zitadel credentials as JSON"
+	errNoProviderConfig              = "no providerConfigRef provided"
+	errGetProviderConfig             = "cannot get referenced ProviderConfig"
+	errTrackUsage                    = "cannot track ProviderConfig usage"
+	errExtractCredentials            = "cannot extract credentials"
+	errUnmarshalCredentials          = "cannot unmarshal github credentials as JSON"
+	errProviderConfigurationBuilder  = "cannot build configuration for terraform provider block"
+	errTerraformProviderMissingOwner = "github provider app_auth needs owner key to be set"
+
+	// provider config variables
+	keyDomain                = "domain"
+	keyInsecure              = "insecure"
+	keyPort                  = "port"
+	keyJwtProfileFile        = "jwt_profile_file"
 )
 
-// TerraformSetupBuilder builds Terraform a terraform.SetupFn function which
-// returns Terraform provider setup configuration
+type zitadelConfig struct {
+	Domain             *string `json:"domain,omitempty"`
+	Insecure           *string `json:"insecure,omitempty"`
+	Port               *string `json:"port,omitempty"`
+	JwtProfileFile     *string `json:"jwt_profile_file,omitempty"`
+}
+
+func terraformProviderConfigurationBuilder(creds zitadelConfig) (terraform.ProviderConfiguration, error) {
+
+	cnf := terraform.ProviderConfiguration{}
+	
+	if creds.Domain != nil {
+		cnf[keyDomain] = *creds.Domain
+	}
+
+	if creds.Insecure != nil {
+		cnf[keyInsecure] = *creds.Insecure
+	}
+	
+	if creds.Port != nil {
+		cnf[keyPort] = *creds.Port
+	}
+
+	if creds.JwtProfileFile != nil {
+		cnf[keyJwtProfileFile] = *creds.JwtProfileFile
+	}
+
+	return cnf, nil
+}
+
+// TerraformSetupBuilder builds Terraform a terraform.SetupFn function which returns Terraform provider setup configuration
 func TerraformSetupBuilder(version, providerSource, providerVersion string) terraform.SetupFn {
 	return func(ctx context.Context, client client.Client, mg resource.Managed) (terraform.Setup, error) {
-		ps := terraform.Setup{
-			Version: version,
-			Requirement: terraform.ProviderRequirement{
-				Source:  providerSource,
-				Version: providerVersion,
-			},
-		}
+		ps := terraform.Setup{}
 
 		configRef := mg.GetProviderConfigReference()
 		if configRef == nil {
 			return ps, errors.New(errNoProviderConfig)
 		}
+
 		pc := &v1beta1.ProviderConfig{}
 		if err := client.Get(ctx, types.NamespacedName{Name: configRef.Name}, pc); err != nil {
 			return ps, errors.Wrap(err, errGetProviderConfig)
@@ -57,16 +89,18 @@ func TerraformSetupBuilder(version, providerSource, providerVersion string) terr
 		if err != nil {
 			return ps, errors.Wrap(err, errExtractCredentials)
 		}
-		creds := map[string]string{}
+
+		creds := zitadelConfig{}
 		if err := json.Unmarshal(data, &creds); err != nil {
 			return ps, errors.Wrap(err, errUnmarshalCredentials)
 		}
 
-		// Set credentials in Terraform provider configuration.
-		ps.Configuration = map[string]any{
-			"zitadel_url": creds["zitadel_url"],
+		ps.Configuration, err = terraformProviderConfigurationBuilder(creds)
+		if err != nil {
+			return ps, errors.Wrap(err, errProviderConfigurationBuilder)
 		}
 
 		return ps, nil
+
 	}
 }
