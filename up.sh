d38cdda674ea4e6f9e7f9656a2c46baf19dd5bfd
reoring <reoring@craftsman-software.com>
2024-08-01 14:51:26 +0900

    chore: update up

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./.cache/tools/darwin_arm64/up-v0.31.0 xpkg push reoring/my-repo:v0.1 -f _output/xpkg/linux_arm64/provider-zitadel-v0.0.0-3.g170af42.xpkg
+./.cache/tools/linux_x86_64/up-v0.31.0 xpkg push reoring/my-repo:v0.3 -f _output/xpkg/linux_amd64/provider-zitadel-v0.0.0-4.gb0bd7c6.xpkg
