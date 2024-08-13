f0d7be1d73223d84856e27359c876046f19fa087
zima <zima@craftsman-software.com>
2024-08-13 01:03:51 +0000

    chore: update up.sh

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./.cache/tools/linux_x86_64/up-v0.31.0 xpkg push reoring/my-repo:v0.15 -f _output/xpkg/linux_amd64/provider-zitadel-v0.0.0-24.gb682c8e.xpkg
+./.cache/tools/linux_x86_64/up-v0.31.0 xpkg push reoring/my-repo:v0.16 -f _output/xpkg/linux_amd64/provider-zitadel-v0.0.0-26.g0f78fcd.xpkg
